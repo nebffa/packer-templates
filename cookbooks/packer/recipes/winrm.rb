c93c5ac1f776ef3938f3abf9ef0849ea76134b74
Ben Lucato <blucato@officeworks.com.au>
2017-09-20 09:49:09 +1000

    Fix WinRM recipe.

--- cookbooks/packer/recipes/winrm.rb
+++ cookbooks/packer/recipes/winrm.rb
@@ -1,16 +1,20 @@
-powershell_script 'install Nuget package provider' do
+powershell_script 'Install NuGet package provider.' do
   code 'Install-PackageProvider -Name NuGet -Force'
   not_if '(Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction SilentlyContinue) -ne $null'
 end
 
+powershell_script 'Configure WinRM' do
+  code 'winrm quickconfig -force -quiet'
+end
+
 %w{xNetworking}.each do |ps_module|
-  powershell_script "install #{ps_module} module" do
+  powershell_script "Install #{ps_module} PowerShell Module" do
     code "Install-Module #{ps_module} -Force"
     not_if "(Get-Module #{ps_module} -list) -ne $null"
   end
 end
 
-dsc_resource 'WinRM' do
+dsc_resource 'WinRM Firewall Rules' do
   resource :xFirewall
   property :name, 'WinRM'
   property :displayName, 'Windows Remote Management public access.'
