4d479988caf690108b5cb4b0c8b57fe1a85ffb32
Ben Lucato <ben.lucato@vibrato.com.au>
2019-01-29 09:44:53 +1100

    Add a builder for Parallels.

--- Execute.ps1
+++ Execute.ps1
@@ -1,7 +1,7 @@
 Param
 (
     [Parameter(Mandatory=$true)]
-    [ValidateSet('HyperV', 'VirtualBox', 'Azure')]
+    [ValidateSet('HyperV', 'VirtualBox', 'Azure', 'Parallels')]
     [String]$BuildType,
 
     [Parameter(Mandatory=$true)]
@@ -26,23 +26,34 @@ Compress-Archive -Path 'cookbooks' `
     -DestinationPath "$PSScriptRoot/bypass_winrm_filecopy/cookbooks.zip" `
     -Force
 
-$templateName = "$($BuildType.ToLower())_$($OperatingSystem.ToLower())"
-if (($BuildType -eq 'HyperV') -or ($BuildType -eq 'VirtualBox'))
-{
-    New-Item -Type Directory -Path 'bypass_winrm_filecopy' -Force | Out-Null
-    Get-ChildItem -Path "$PSScriptRoot\cookbooks" -Filter "*.vhdx" | Remove-Item -Force
-    
-    packer build -force -var-file="$PSScriptRoot/templates/$templateName.json" `
-        "$PSScriptRoot/templates/virtualbox-windows.json" `
-        
-    Test-LastExitCode
+Push-Location $PSScriptRoot
 
-    Write-Output "Adding the freshly created box to Vagrant..."
-    vagrant box add (Join-Path $env:IMAGES_DIR "$templateName.box") `
-        --name $OperatingSystem.ToLower().Replace('_', '-') --force
-}
-elseif ($BuildType -eq 'Azure')
+try 
 {
-    #az storage blob upload --container-name packer --name cookbooks.zip --file ./cookbooks.zip --account-name vibratopacker --account-key "dzofr2vDemawYCoXodN+LjH9N7MveO73XvK0Zuuy+TcUekbTjTXJ3qtaOlyaC+L9Mf1JvRhgQzo/q2Xr2LscSQ=="
-    packer build -force "$PSScriptRoot/templates/$templateName.json"
+    $templateName = "$($BuildType.ToLower())_$($OperatingSystem.ToLower())"
+    if (($BuildType -eq 'HyperV') -or ($BuildType -eq 'VirtualBox') -or ($BuildType -eq 'Parallels'))
+    {
+        New-Item -Type Directory -Path 'bypass_winrm_filecopy' -Force | Out-Null
+        Get-ChildItem -Path "$PSScriptRoot\cookbooks" -Filter "*.vhdx" | Remove-Item -Force
+        
+        packer build -force -var-file="$PSScriptRoot/templates/$templateName.json" `
+            -only="parallels-iso" `
+            "$PSScriptRoot/templates/virtualbox-windows.json"
+            
+            
+        Test-LastExitCode
+
+        Write-Output "Adding the freshly created box to Vagrant..."
+        vagrant box add (Join-Path $env:IMAGES_DIR "$templateName.box") `
+            --name $OperatingSystem.ToLower().Replace('_', '-') --force
+    }
+    elseif ($BuildType -eq 'Azure')
+    {
+        #az storage blob upload --container-name packer --name cookbooks.zip --file ./cookbooks.zip --account-name vibratopacker --account-key "dzofr2vDemawYCoXodN+LjH9N7MveO73XvK0Zuuy+TcUekbTjTXJ3qtaOlyaC+L9Mf1JvRhgQzo/q2Xr2LscSQ=="
+        packer build -force "$PSScriptRoot/templates/$templateName.json"
+    }
+}
+finally
+{
+    Pop-Location
 }
--- scripts/packer_shutdown.ps1
+++ scripts/packer_shutdown.ps1
@@ -1,8 +1,3 @@
 $ErrorActionPreference = 'Stop'
 
-
-winrm set winrm/config/service/auth '@{Basic="false"}'
-winrm set winrm/config/service '@{AllowUnencrypted="false"}'
-Remove-NetFirewallRule -DisplayName 'WinRM-HTTP'
-
 C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /unattend:'A:/Post_Autounattend.xml' /quiet /shutdown
